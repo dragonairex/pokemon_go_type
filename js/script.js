725e4edea77349bad5f4c2ed466670766dd536db
Derek <dragonairex@gmail.com>
2016-11-15 13:16:49 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 window.onload = function() {
-  console.log('loaded.');
+  $("#table td").on('mouseover',function(){
+  	var col = $(this).index();
+  	var row = $(this).parent().index();
+
+  	var colClass=$("#table tr:first-child th:nth-child("+(col+1)+")").attr("class");
+  	var rowClass=$("#table tr:nth-child("+(row+1)+") th").attr("class");
+
+  	$("#table td").removeClass();
+  	$("#table tr:nth-child("+(row+1)+") td").not(this).addClass(rowClass);
+  	$("#table tr td:nth-child("+(col+1)+")").not(this).addClass(colClass);
+  	$(this).addClass("crosshair");
+  });
+  $("#table").on('mouseleave',function(){
+  	$("#table td").removeClass();
+  });
 };
